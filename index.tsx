9b04fba36215f7f30fdb4646a9abf3f51b14602b
Mohamed <elouarghii@gmail.com>
2025-07-19 17:34:29 +0200

    MReserv V4.12

--- index.tsx
+++ index.tsx
@@ -7,6 +7,7 @@ import { ThemeProvider } from './contexts/ThemeContext';
 import { ConfirmationProvider } from './contexts/ConfirmationContext';
 import ErrorBoundary from './components/ErrorBoundary';
 
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,9 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      css: {
-        postcss: {
-          plugins: [
-            tailwindcss,
-            autoprefixer,
-          ],
-        },
-      },
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
